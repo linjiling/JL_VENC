58458f355359c6fd61de9801ddb3a083bbfb5d46
linjiling <1193615975@qq.com>
2026-03-12 15:10:29 +0800

    Rename and add H264 register operation functions

--- jl_hal_intf_h264.h
+++ jl_hal_intf_h264.h
@@ -2,4 +2,11 @@
 td_bool jl_hal_h264_sse_support(td_void);
 
 /* vedu内存计算接口 */
-jl_hal_h264e_get_ai_mask_stride(td_u32 pic_width);
+jl_hal_h264_get_ai_mask_stride(td_u32 pic_width);
+
+/* vedu寄存器Node内存操作接口 */
+td_void jl_hal_h264_init_regs_node(td_s32 chn_id);
+td_void jl_hal_h264_update_regs_node(td_s32 chn_id);
+td_void jl_hal_h264_read_regs_node(td_s32 chn_id);
+
+/* vedu寄存器直接操作接口 */
