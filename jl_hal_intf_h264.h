a9bcd0fc347118d4a9f13f6e1317fb93e54f8dc2
linjiling <1193615975@qq.com>
2026-03-12 15:49:21 +0800

    Update H264 HAL interface for channel support

--- jl_hal_intf_h264.h
+++ jl_hal_intf_h264.h
@@ -1,10 +1,10 @@
-/* 特别说明：hal层不独立成模块，代码附属于各个模块中，因此部分可能被多个模块调用的接口需要做到上下文无关性或者每个上下文对应一个接口 */
+/* 特别说明：hal层独立成模块 形成ot_venc_hal.ko */
 
 /* vedu能力查询接口 */
-td_bool jl_hal_h264_sse_support(td_void);
+td_bool jl_hal_h264_sse_support(td_s32 chn_id, td_void);
 
 /* vedu内存计算接口 */
-jl_hal_h264_get_ai_mask_stride(td_u32 pic_width);
+td_u32 jl_hal_h264_get_ai_mask_stride(td_s32 chn_id, td_u32 pic_width);
 
 /* vedu寄存器Node内存操作接口 */
 td_void jl_hal_h264_init_regs_node(td_s32 chn_id);
