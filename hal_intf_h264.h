5f40ac41d005bd3d7209e181b5629aeae2367eb8
linjiling <1193615975@qq.com>
2026-03-12 14:44:49 +0800

    Update and rename hal_intf_h264.h to jl_hal_intf_h264.h

--- hal_intf_h264.h
+++
@@ -1,5 +0,0 @@
-/* vedu能力查询接口 */
-td_bool jhal_h264_sse_support(td_void);
-
-/* vedu内存计算接口 */
-jhal_h264e_get_ai_mask_stride(td_u32 pic_width);
---
+++ jl_hal_intf_h264.h
@@ -0,0 +1,5 @@
+/* vedu能力查询接口 */
+td_bool jl_hal_h264_sse_support(td_void);
+
+/* vedu内存计算接口 */
+jl_hal_h264e_get_ai_mask_stride(td_u32 pic_width);
