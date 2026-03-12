96fc6677f051b0cb345d7c9248fb18b31d367bbb
linjiling <1193615975@qq.com>
2026-03-12 16:12:13 +0800

    Update comment for hal layer module explanation

--- jl_hal_intf_h264.h
+++ jl_hal_intf_h264.h
@@ -1,4 +1,4 @@
-/* 特别说明：hal层独立成模块 形成ot_venc_hal.ko */
+/* 特别说明：hal层独立成模块 形成ot_venc_hal.ko， 这种方法可以使得ot_venc.ko和ot_h264e.ko都能访问一套代码，不必做两套代码 */
 
 /* vedu能力查询接口 */
 td_bool jl_hal_h264_sse_support(td_s32 chn_id, td_void);
