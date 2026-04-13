8b16712ed8333faa4f99f5aebe3b674220750d54
linjiling <1193615975@qq.com>
2026-04-13 10:43:51 +0800

    Add ioctl command enum for video encoding
    Add ioctl command enumeration for video encoding channel operations.

--- jl_venc_l1_intf.h
+++ jl_venc_l1_intf.h
@@ -1,6 +1,14 @@
 #ifndef VENC_L1_INTF_H
 #define VENC_L1_INTF_H
 
+typedef enum {
+    IOC_VENC_CREATE_CHN = 0,
+    IOC_VENC_DESTROY_CHN,
+    IOC_VENC_START_CHN,
+    IOC_VENC_STOP_CHN,
+    IOC_VENC_RESET_CHN,
+} jl_venc_ioctl_cmd;
+
 /* 提供给给CHNL模块调用的接口 */
 td_s32 jl_venc_query_task_ready(td_s32 chn_id, td_void *info);
 td_s32 jl_venc_handle_interrupt(td_s32 chn_id, td_void *info);
